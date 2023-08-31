f359cb4345742c54f7d0cd45282bd72fe75b0496
fazziclay <fazziclay@gmail.com>
2023-08-31 20:38:50 +0300

    Initial commit

---
+++ src/com/fazziclay/warningrose/Rose.java
@@ -0,0 +1,99 @@
+package com.fazziclay.warningrose;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.function.Supplier;
+
+public class Rose {
+    private static final int MILLISECOND = 1;
+    private static final int SECOND = MILLISECOND * 1000;
+    private static final int MINUTE = SECOND * 60;
+    private static final int HOUR = MINUTE * 60;
+    private static final int DAY = HOUR * 24;
+
+    private final String[] weekdays = new String[] {
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Unknown 1",
+            "Unknown 2",
+            "Unknown 3",
+            "Unknown 4",
+            "Unknown 5",
+            "Unknown 6",
+            "Unknown 7",
+    };
+    private final long startMillis;
+    private final Calendar startCalendar; // unused...
+
+    private final long endMillis;
+    private final Calendar endCalendar;
+
+    private final Supplier<Long> currentTimeSupplier;
+
+    public Rose(long startMillis, long endMillis, Supplier<Long> currentTimeSupplier) {
+        this.startMillis = startMillis;
+        this.startCalendar = new GregorianCalendar();
+        this.startCalendar.setTimeInMillis(startMillis);
+
+        this.endMillis = endMillis;
+        this.endCalendar = new GregorianCalendar();
+        this.endCalendar.setTimeInMillis(endMillis);
+
+
+        this.currentTimeSupplier = currentTimeSupplier;
+    }
+
+    public long elapsedTotal() {
+        return endMillis - currentMs();
+    }
+
+    public float elapsedDays() {
+        return (float) elapsedTotal() / DAY;
+    }
+
+    public int elapsedYears() {
+        final var currentCalendar = new GregorianCalendar();
+        currentCalendar.setTimeInMillis(currentMs());
+
+        return endCalendar.get(Calendar.YEAR) - currentCalendar.get(Calendar.YEAR);
+    }
+
+    public float elapsedWeeks() {
+        return (elapsedDays() + (endCalendar.get(Calendar.DAY_OF_WEEK) - 1)) / 7F;
+    }
+
+    public int elapsedTotalHours() {
+        long durationInMillis = elapsedTotal();
+
+        return (int) ((durationInMillis / (1000 * 60 * 60)) % 24) + ((int)elapsedDays() * 24);
+    }
+
+    public double endlessPercentage() {
+        double b = endMillis - startMillis;
+        return elapsedTotal() / b * 100D;
+    }
+
+    private long currentMs() {
+        return currentTimeSupplier.get();
+    }
+
+    public String elapsedSummaryText() {
+        long durationInMillis = elapsedTotal();
+
+        int h = (int) ((durationInMillis / (1000 * 60 * 60)) % 24);
+        int m = (int) ((durationInMillis / (1000 * 60)) % 60);
+        int s = (int) ((durationInMillis / 1000) % 60);
+        return "%s days %02d:%02d:%02d".formatted((int)Math.floor(elapsedDays()), h, m, s);
+    }
+
+    public String getCurrentWeekday() {
+        final GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTimeInMillis(currentMs());
+        return weekdays[gregorianCalendar.get(Calendar.DAY_OF_WEEK) - 1];
+    }
+}
---
+++ src/com/fazziclay/warningrose/WarningRose.java
@@ -0,0 +1,55 @@
+package com.fazziclay.warningrose;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class WarningRose {
+    @Deprecated // NOT USE THIS!!!!!!!!!! THIS MAYBE DELETED!!!!!
+    public static final long EGE_PREPARE_END_MILLIS = 1714510800L * 1000L; // Wed May 01, 2024 00:00:00 GMT+0300 (Moscow Standard Time)
+
+    @Deprecated // NOT USE THIS!!!!!!!!!! THIS MAYBE DELETED!!!!!
+    public static final long TEN_CLASS_START = 1661979600L * 1000L; // Thu Sep 01 2022 00:00:00 GMT+0300 (Moscow Standard Time)
+
+
+    private static final WarningRose INSTANCE = new WarningRose();
+
+    private final Rose rose;
+
+    public static void main(String[] args) {
+        INSTANCE.run();
+    }
+
+    private WarningRose() {
+        rose = new Rose(TEN_CLASS_START, EGE_PREPARE_END_MILLIS, System::currentTimeMillis);
+    }
+
+    private void run() {
+        System.out.println("=== Warning Rose ===");
+        System.out.println(rose.elapsedSummaryText());
+        System.out.println("Weeks: " + rose.elapsedWeeks());
+        System.out.println((float) rose.endlessPercentage() + "%");
+
+        JFrame frame = new JFrame("Warning Rose...");
+        frame.setBackground(Color.GRAY);
+        frame.setAutoRequestFocus(true);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(400, 190);
+
+        JTextArea text = new JTextArea();
+        text.setEditable(false);
+        text.setMargin(new Insets(10, 20, 10, 20));
+        text.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 30));
+        frame.getContentPane().add(text);
+
+        while (frame.isVisible()) {
+            text.setText(rose.elapsedSummaryText() + "\n | " + rose.elapsedTotalHours() + " hours" + "\n | " + Math.round(rose.elapsedWeeks()) + " weeks (C: " + rose.getCurrentWeekday() + ")" + "\n | " + (float) rose.endlessPercentage() + "%");
+            text.invalidate();
+            text.validate();
+            text.repaint();
+            try {
+                Thread.sleep(frame.hasFocus() ? 100 : 1000);
+            } catch (InterruptedException ignored) {}
+        }
+    }
+}
